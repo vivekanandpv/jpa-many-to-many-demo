e3aad1a63ae850a56540fedd5f56416c0e840d2e
Vivekanand P V <vivekanandpv@outlook.com>
2024-05-31 15:19:11 +0530

    partial 3

--- src/main/java/com/example/jpamanytomanydemo/apis/RoleApi.java
+++ src/main/java/com/example/jpamanytomanydemo/apis/RoleApi.java
@@ -1,4 +1,29 @@
 package com.example.jpamanytomanydemo.apis;
 
+import com.example.jpamanytomanydemo.services.AppRoleService;
+import com.example.jpamanytomanydemo.viewmodels.AppRoleCreateViewModel;
+import com.example.jpamanytomanydemo.viewmodels.AppRoleViewModel;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/roles")
 public class RoleApi {
+    private final AppRoleService roleService;
+
+    public RoleApi(AppRoleService roleService) {
+        this.roleService = roleService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AppRoleViewModel>> get() {
+        return ResponseEntity.ok(roleService.get());
+    }
+
+    @PostMapping
+    public ResponseEntity<AppRoleViewModel> create(@RequestBody AppRoleCreateViewModel viewModel) {
+        return ResponseEntity.ok(roleService.create(viewModel));
+    }
 }
--- src/main/java/com/example/jpamanytomanydemo/apis/UserApi.java
+++ src/main/java/com/example/jpamanytomanydemo/apis/UserApi.java
@@ -1,5 +1,36 @@
 package com.example.jpamanytomanydemo.apis;
 
+import com.example.jpamanytomanydemo.exceptions.RecordNotFoundException;
+import com.example.jpamanytomanydemo.services.AppUserService;
+import com.example.jpamanytomanydemo.viewmodels.AppUserCreateViewModel;
+import com.example.jpamanytomanydemo.viewmodels.AppUserViewModel;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("api/v1/users")
 public class UserApi {
+    private final AppUserService userService;
+
+    public UserApi(AppUserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AppUserViewModel>> get() {
+        return ResponseEntity.ok(userService.get());
+    }
+
+    @PostMapping
+    public ResponseEntity<AppUserViewModel> create(@RequestBody AppUserCreateViewModel viewModel) {
+        return ResponseEntity.ok(userService.create(viewModel));
+    }
+
+    @ExceptionHandler(RecordNotFoundException.class)
+    public ResponseEntity<?> handleRecordNotFoundException(RecordNotFoundException exception) {
+        return ResponseEntity.badRequest().body(Map.of("error", exception.getMessage()));
+    }
 }

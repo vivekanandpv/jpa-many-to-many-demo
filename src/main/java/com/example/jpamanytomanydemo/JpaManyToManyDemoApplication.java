907734f0468ecb10ef9c3b86a12e681783053a98
Vivekanand P V <vivekanandpv@outlook.com>
2024-05-31 12:53:07 +0530

    Initial commit

---
+++ src/main/java/com/example/jpamanytomanydemo/JpaManyToManyDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpamanytomanydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaManyToManyDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaManyToManyDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jpamanytomanydemo/JpaManyToManyDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpamanytomanydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaManyToManyDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
